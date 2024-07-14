6f9dbb6cc3ac60951a2e2da91ef74c87b2ef2a0c
ilkermansur <59640302+ilkermansur@users.noreply.github.com>
2024-07-14 22:49:02 +0300

    Update and rename streamlit_app.py to Configuration_Tool.py

---
+++ Configuration_Tool.py
@@ -0,0 +1,16 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+"""
+# Welcome to Configuration App!
+
+"""
+
+st.title ("Configuration Tool")
+
+st.markdown("---")
+st.markdown("#### Instructions")
+
+
